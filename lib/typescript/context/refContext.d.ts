9194303fd7179e90c692d8f2ea43dd909213b2e8
hakansaglam <hakan.saglam@qconnex.com>
2023-01-05 15:00:44 +0100

    lib folder added

---
+++ lib/typescript/context/refContext.d.ts
@@ -0,0 +1,19 @@
+import React from "react";
+import { FlashList } from "@shopify/flash-list";
+import Animated, { WithSpringConfig } from "react-native-reanimated";
+import { CellData, DraggableFlatListProps } from "../types";
+declare type RefContextValue<T> = {
+    propsRef: React.MutableRefObject<DraggableFlatListProps<T>>;
+    animationConfigRef: React.MutableRefObject<WithSpringConfig>;
+    cellDataRef: React.MutableRefObject<Map<string, CellData>>;
+    keyToIndexRef: React.MutableRefObject<Map<string, number>>;
+    containerRef: React.RefObject<Animated.View>;
+    flatlistRef: React.RefObject<FlashList<T>> | React.ForwardedRef<FlashList<T>>;
+    scrollViewRef: React.RefObject<Animated.ScrollView>;
+};
+export default function RefProvider<T>({ children, flatListRef, }: {
+    children: React.ReactNode;
+    flatListRef?: React.ForwardedRef<FlashList<T>> | null;
+}): JSX.Element;
+export declare function useRefs<T>(): RefContextValue<T>;
+export {};
